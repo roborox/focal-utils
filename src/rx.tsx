ce9789e586eacc434170c790ae646d7109342fba
Roborox dev <dev@roborox.org>
2020-02-24 22:53:45 +0300

    feat(*): Rx component

---
+++ src/rx.tsx
@@ -0,0 +1,15 @@
+import { Observable } from "rxjs"
+import { useRx } from "./use-rx"
+
+interface Props<T> {
+	value: Observable<T>
+	children: (t: T) => React.ReactElement
+}
+
+export function Rx<T>({value, children}: Props<T>) {
+	const simple = useRx(value)
+	if (simple !== null) {
+		return children(simple)
+	}
+	return null
+}
