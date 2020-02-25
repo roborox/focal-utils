01b4175baf945776430bd8b80162baa057fe831c
Roborox dev <dev@roborox.org>
2020-02-25 09:35:40 +0300

    feat(*): byKeyPartial

--- src/lenses/by-key.ts
+++ src/lenses/by-key.ts
@@ -1,5 +1,5 @@
 import { Map } from "immutable"
-import { Lens } from "@grammarly/focal"
+import { Lens, Option } from "@grammarly/focal"
 
 export function byKey<K, V>(k: K): Lens<Map<K, V>, V> {
 	return Lens.create(
@@ -8,11 +8,22 @@ export function byKey<K, V>(k: K): Lens<Map<K, V>, V> {
 	)
 }
 
-function setWithCheck<K, V>(key: K, value: V): (map: Map<K, V>) => Map<K, V> {
+export function byKeyPartial<K, V>(k: K): Lens<Map<K, V>, Option<V>> {
+	return Lens.create(
+		it => it.get(k),
+		(it, map) => setWithCheck(k, it)(map),
+	)
+}
+
+function setWithCheck<K, V>(key: K, value: Option<V>): (map: Map<K, V>) => Map<K, V> {
 	return map => {
 		const current = map.get(key)
 		if (current !== value) {
-			return map.set(key, value)
+			if (value === undefined) {
+				return map.remove(key)
+			} else {
+				return map.set(key, value)
+			}
 		} else {
 			return map
 		}
