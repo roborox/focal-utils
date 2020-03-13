57f73c1f839a3a451438c36ebf46af98eef5b2f4
Roborox dev <dev@roborox.org>
2020-03-13 22:36:06 +0300

    fix(*): better ListLoader implementation

--- src/loading/list-loader.tsx
+++ src/loading/list-loader.tsx
@@ -1,10 +1,9 @@
 import React from "react"
 import { Atom } from "@grammarly/focal"
-import { map } from "rxjs/operators"
-import { useRx } from "@roborox/rxjs-react/build/use-rx"
 import { LoaderCases } from "./domain"
 import { LoadableListState } from "./loadable-list"
 import { Loader } from "./loader"
+import { LoadingStatus } from "@roborox/rxjs-react/build/to-rx"
 
 export type ListLoaderProps<D, C> = {
 	state: Atom<LoadableListState<D, C>>,
@@ -14,22 +13,10 @@ export type ListLoaderProps<D, C> = {
 export const ListLoader = <D, C>({
 	state, children, ...restProps
 }: ListLoaderProps<D, C>): React.ReactElement | null => {
-	const isFirstTimeLoading = useRx(state.pipe(map((state) => {
-		return state.items.length === 0
-	})))
-
-	if (isFirstTimeLoading !== null) {
-		if (isFirstTimeLoading) {
-			return (
-				<Loader status={state.pipe(map((state) => state.loadingState.status))} {...restProps}>
-					{children}
-				</Loader>
-			)
-		}
-
-		return children
-	}
-	return null
+	const firstLoadStatus = state.view<LoadingStatus>(
+		state => state.items.length === 0 ? state.loadingState.status : {status: "success"},
+	)
+	return <Loader status={firstLoadStatus} {...restProps}>{children}</Loader>
 }
 
 ListLoader.displayName = "ListLoader"
